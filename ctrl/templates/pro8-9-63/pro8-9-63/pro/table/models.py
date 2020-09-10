1246501e9061e6b0d2ff00d4556cc93faeb24e03
showkuay97 <mameesukh777@hotmail.com>
2020-09-10 15:10:30 +0700

    uploads

---
+++ ctrl/templates/pro8-9-63/pro8-9-63/pro/table/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+class repair_cmpt(models.Model):
+    class_room = models.CharField(max_length=3)
+    slug_repair = models.SlugField(max_length=255)
+    No_cmpt = models.CharField(max_length=2)
+    img_cmpt = models.ImageField()
+    stat_cmpt = models.CharField(max_length=255)
+    detail_repair = models.CharField(max_length=255)
+    date_input = models.DateField(auto_now_add=True)
+    date_output = models.DateField(auto_now_add=True)
+    user_report = models.CharField(max_length=255)
+    check_repair =models.CharField(max_length=255)
+    repair = models.CharField(max_length=255)
+    def __str__(self):
+        return self.slug_repair
+
+class cmpt_detail(models.Model):
+    class_room = models.CharField(max_length=255)
+    slug_detail = models.SlugField(max_length=255)
+    No_cmpt = models.CharField(max_length=2)
+    board = models.CharField(max_length=255)
+    cpu = models.CharField(max_length=255)
+    ram = models.CharField(max_length=255)
+    hdd = models.CharField(max_length=255)
+    ssd = models.CharField(max_length=255)
+    psu = models.CharField(max_length=255)
+    case = models.CharField(max_length=255)
+    monitor = models.CharField(max_length=255)
+    gpu = models.CharField(max_length=255)
+    count_cpu = models.CharField(max_length=255)
+    count_ram = models.CharField(max_length=255)
+    count_hdd = models.CharField(max_length=255)
+    count_ssd = models.CharField(max_length=255)
+    count_psu = models.CharField(max_length=255)
+    count_case = models.CharField(max_length=255)
+    count_monitor = models.CharField(max_length=255)
+    count_gpu = models.CharField(max_length=255)
+    count_all = models.IntegerField()
+    def __str__(self):
+        return self.slug_detail
+
+class report(models.Model):
+    class_room = models.CharField(max_length=255)
+    slug_report = models.SlugField(max_length=255)
+    No_cmpt = models.CharField(max_length=2)
+    detail_repair = models.CharField(max_length=255)
+    device_repair = models.CharField(max_length=255)
+    def __str__(self):
+        return self.slug_report
---
+++ ctrl/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def Login(request):
+    return render(request,'Login.html')
+
+def pcstudent(request):
+    return render(request,'pcstudent.html')
+
+def pcteacher(request):
+    return render(request,'pcteacher.html')
+
+def register(request):
+    return render(request,'regis.html')
+def Dashboard(request):
+    return render(request,'Dashboard.html')
