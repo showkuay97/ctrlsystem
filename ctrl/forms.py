1146550a34984abe2f5ebd61270abeb19b0bf5ce
showkuay97 <mameesukh777@hotmail.com>
2020-09-20 13:23:48 +0700

    kuy

--- ctrl/forms.py
+++ ctrl/forms.py
@@ -25,28 +25,28 @@ class opsForm(forms.Form):
     )
     
 class repiarForm(forms.Form):
-    text  = {
-        'field1','field2','field3','field4','field5',
-    }
-    widgets={
-        'field1': forms.CheckboxInput(attrs={'id':'lb_repair01',}),
-        'field2': forms.CheckboxInput(attrs={'id':'lb_repair02',}),
-        'field3': forms.CheckboxInput(attrs={'id':'lb_repair03',}),
-        'field4': forms.CheckboxInput(attrs={'id':'lb_repair04',}),
-        'field5': forms.CheckboxInput(attrs={'id':'lb_repair05',}),
-    }
-    # field1 = forms.BooleanField(
-    #     required=False,
-    #     label='เปิดคอมพิวเตอร์มาแล้วขึ้นหน้าจอฟ้า',
-    #     initial=False,
-    #     widget=forms.CheckboxInput(attrs={'id':'lb_repair01',})
-    # )
-    # field2 = forms.BooleanField(
-    #     required=False,
-    #     label='หน้าจอคอมพิวเตอร์เปิดไม่ติด',
-    #     initial=False,
-    #     widget=forms.CheckboxInput(attrs={'id':'lb_repair01'})
-    # )
+    # fields  = {
+    #     'field1','field2','field3','field4','field5',
+    # }
+    # widgets={
+    #     'field1': forms.TextInput(attrs={'placeholder':'kuy'})
+    #     # 'field2': forms.CheckboxInput(attrs={'id':'lb_repair02',}),
+    #     # 'field3': forms.CheckboxInput(attrs={'id':'lb_repair03',}),
+    #     # 'field4': forms.CheckboxInput(attrs={'id':'lb_repair04',}),
+    #     # 'field5': forms.CheckboxInput(attrs={'id':'lb_repair05',}),
+    # }
+    field1 = forms.BooleanField(
+        required=False,
+        label='เปิดคอมพิวเตอร์มาแล้วขึ้นหน้าจอฟ้า',
+        initial=False,
+        widget=forms.CheckboxInput(attrs={'id':'lb_repair01',})
+    )
+    field2 = forms.BooleanField(
+        required=False,
+        label='หน้าจอคอมพิวเตอร์เปิดไม่ติด',
+        initial=False,
+        widget=forms.CheckboxInput(attrs={'id':'lb_repair01'})
+    )
     # field3 = forms.BooleanField(
     #     required=False,
     #     label='คอมพิวเตอร์มีเสียงร้องตอนเปิดเครื่อง',
@@ -62,3 +62,4 @@ class input_detail(forms.Form):
     # https://pypi.org/project/django-bootstrap-modal-forms/2.0.0/
     # https://stackoverflow.com/questions/37170465/how-to-get-value-checkbox-on-modal-bootstrap
     # boolean Field   https://www.youtube.com/watch?v=C4P5fiAC7QA&ab_channel=DJanGO
+    # git hub https://github.com/showkuay97/ctrlsystem.git
